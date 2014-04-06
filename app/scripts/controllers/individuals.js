421145fffcb2108f4b2dff2524fab827a9fa5773
Hintea Dan Alexandru <hinteadan@gmail.com>
2014-04-06 21:02:36 +0300

    Add remove option for registered participants plus populate by default with some dummy participants (until we have some sort of storage)

--- app/scripts/controllers/individuals.js
+++ app/scripts/controllers/individuals.js
@@ -5,7 +5,18 @@
 	angular.module('scoreKeeperSiteApp')
 	.controller('IndividualsCtrl', ['$scope', function ($scope) {
 
-		var individuals = [new Individual('Dan', 'Hintea')];
+		var individuals = [
+			new Individual('Dan', 'Hintea'),
+			new Individual('Roger', 'Federer'),
+			new Individual('Rafael', 'Nadal'),
+			new Individual('Stanislas', 'Wawrinka'),
+			new Individual('Novak', 'Djokovic'),
+			new Individual('Eugenie', 'Bouchard'),
+			new Individual('Caroline', 'Wozniaki'),
+			new Individual('Maria', 'Sharapova'),
+			new Individual('Serena', 'Williams'),
+			new Individual('Andy', 'Murray')
+			];
 
 		function addIndividualNamed(firstName, lastName){
 			if(!lastName){
@@ -14,8 +25,17 @@
 			individuals.push(new Individual(firstName, lastName));
 		}
 
+		function removeIndividual(person){
+			var index = individuals.indexOf(person);
+			if(index < 0){
+				return;
+			}
+			individuals.splice(index, 1);
+		}
+
 		$scope.people = function(){ return individuals; };
 		$scope.add = addIndividualNamed;
+		$scope.remove = removeIndividual;
 
 	}]);
 
