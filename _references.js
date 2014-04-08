1ca3189d17d86329bd406782d3fbe5de962e519c
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-04-08 11:24:37 +0300

    OD: Team definition screen

---
+++ _references.js
@@ -0,0 +1,1 @@
+﻿/// <reference path="app/bower_components/h.scoreKeeper/h.scoreKeeper.js" />
--- app/scripts/controllers/teams.js
+++ app/scripts/controllers/teams.js
@@ -1,12 +1,25 @@
-(function(angular, undefined){
+(function(angular, Individual, undefined){
 
 	'use strict';
 
 	angular.module('scoreKeeperSiteApp')
-	.controller('TeamsCtrl', [function () {
+	.controller('TeamsCtrl', ['$scope', function ($scope) {
 
-		
+		var individuals = [
+			new Individual('Dan', 'Hintea'),
+			new Individual('Roger', 'Federer'),
+			new Individual('Rafael', 'Nadal'),
+			new Individual('Stanislas', 'Wawrinka'),
+			new Individual('Novak', 'Djokovic'),
+			new Individual('Eugenie', 'Bouchard'),
+			new Individual('Caroline', 'Wozniaki'),
+			new Individual('Maria', 'Sharapova'),
+			new Individual('Serena', 'Williams'),
+			new Individual('Andy', 'Murray')
+			];
+
+		$scope.people = function(){ return individuals; };
 
 	}]);
 
-}).call(this, this.angular);
+}).call(this, this.angular, this.H.ScoreKeeper.Individual);
