bc82a2234148909c4f6af87c483251c528f5bb4b
Hintea Dan Alexandru <hinteadan@gmail.com>
2014-04-12 10:54:35 +0300

    Mark teams as assigned

--- _references.js
+++ _references.js
@@ -1,1 +1,2 @@
 ﻿/// <reference path="app/bower_components/h.scoreKeeper/h.scoreKeeper.js" />
+/// <reference path="app/bower_components/lodash/dist/lodash.js" />
--- app/scripts/controllers/teams.js
+++ app/scripts/controllers/teams.js
@@ -1,6 +1,10 @@
-(function(angular, Individual, Team, sks, undefined){
+(function(angular, Individual, Team, sks, _, undefined){
 
 	'use strict';
+
+	function IndividualViewData(){
+		this.team = null;
+	}
 
 	angular.module('scoreKeeperSiteApp')
 	.controller('TeamsCtrl', ['$scope', function ($scope) {
@@ -19,8 +23,17 @@
 		    ],
 	        teams = [];
 
+        function resetIndividualsAssignedTeam(){
+        	_.each(individuals, function(i){ i.viewData.team = null; });
+        }
+
 		function generateRandomTeamsOf(memberCount) {
 		    teams = new sks.RandomPartiesGenerator(individuals).partiesOf(memberCount);
+		    _.each(teams, function(t){
+	    		_.each(t.individuals(), function(i){
+	    			i.viewData.team = t;
+	    		});
+		    });
 		}
 
 		function generateEmptyTeamsOf(memberCount) {
@@ -29,13 +42,22 @@
 		    for (var i = 1; i <= teamCount; i++) {
 		        teams.push(new Team('Team ' + i));
 		    }
+		    resetIndividualsAssignedTeam();
+		}
+
+		function extendIndividualWithViewData(individual){
+			individual.viewData = new IndividualViewData();
+		}
+
+		function initialize(){
+			_.each(individuals, extendIndividualWithViewData);
 		}
 
 		$scope.people = function () { return individuals; };
 		$scope.teams = function () { return teams; };
 		$scope.randomOf = generateRandomTeamsOf;
 		$scope.emptyOf = generateEmptyTeamsOf;
-
+		initialize();
 	}]);
 
-}).call(this, this.angular, this.H.ScoreKeeper.Individual, this.H.ScoreKeeper.Party, this.H.ScoreKeeper.Sattelites);
+}).call(this, this.angular, this.H.ScoreKeeper.Individual, this.H.ScoreKeeper.Party, this.H.ScoreKeeper.Sattelites, this._);
