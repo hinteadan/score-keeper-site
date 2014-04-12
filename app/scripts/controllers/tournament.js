e5eb517fbfd865ef8e0fac2360f9f8004be89f01
Hintea Dan Alexandru <hinteadan@gmail.com>
2014-04-12 14:05:23 +0300

    Scaffold tournament page

---
+++ app/scripts/controllers/tournament.js
@@ -0,0 +1,10 @@
+(function(angular, undefined){
+
+	'use strict';
+
+	angular.module('scoreKeeperSiteApp')
+	.controller('TournamentCtrl', ['$scope', function ($scope) {
+		$scope.dummy = null;
+	}]);
+
+}).call(this, this.angular);
