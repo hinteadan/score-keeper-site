91070ba289cae1e38170180d43db59169e0fcd70
Hintea Dan Alexandru <hinteadan@gmail.com>
2014-04-16 00:24:11 +0300

    Add ngRoute and configure main paths

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,11 @@
 'use strict';
 
 angular
-  .module('scoreKeeperSiteApp', []);
+    .module('scoreKeeperSiteApp', ['ngRoute'])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/step1', { templateUrl: 'views/individuals.tmpl.html', controller: 'IndividualsCtrl' })
+            .when('/step2', { templateUrl: 'views/teams.tmpl.html', controller: 'TeamsCtrl' })
+            .when('/step3', { templateUrl: 'views/tournament.tmpl.html', controller: 'TournamentCtrl' })
+            .otherwise({ redirectTo: '/step1' });
+    }]);
