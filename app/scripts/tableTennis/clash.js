7e523b7b4a9b8e2d2e236a2fb7f4db67797a44fe
Hintea Dan Alexandru <hinteadan@gmail.com>
2014-05-31 21:44:28 +0300

    Define Clash

--- app/scripts/tableTennis/clash.js
+++ app/scripts/tableTennis/clash.js
@@ -1,16 +1,18 @@
 ﻿(function (angular, k) {
 	'use strict';
 
-	function ClashDetails(pointsToWin, firstToServe) {
+	function ClashDetails(pointsToWin, firstToServe, firstToReceive) {
 		this.pointsToWin = pointsToWin || 20;
 		this.firstToServe = firstToServe;
+		this.firstToReceive = firstToReceive;
+		this.serveChangeAfter = 5;
 	}
 
 	angular.module('ScoreKeeper.TableTennis')
 	.service('Clash', [function () {
 		var parties = [
-				new k.Party(),
-				new k.Party()
+				new k.Party('Team Awesome').addMembers([new k.Individual('Hintea', 'Dan'), new k.Individual('Pascalau', 'Anca')]),
+				new k.Party('Team D&G').addMembers([new k.Individual('Pacurar', 'Georgiana'), new k.Individual('Mis', 'Diana Alina')])
 			],
 			clashDetails = new ClashDetails();
 
--- app/scripts/tableTennis/defineClashCtrl.js
+++ app/scripts/tableTennis/defineClashCtrl.js
@@ -1,9 +1,18 @@
-﻿(function (angular, k) {
+﻿(function (angular, k, _) {
 	'use strict';
 
 	angular.module('ScoreKeeper.TableTennis')
 		.controller('defineClash', ['$scope', 'Clash', function ($scope, clash) {
 			$scope.clashDetails = clash.details;
+			$scope.parties = clash.parties;
+			$scope.receivingParty = function () {
+				if (!clash.details.firstToServe) {
+					return null;
+				}
+				return _.find(clash.parties, function (p) {
+					return !_.contains(p.individuals, clash.details.firstToServe);
+				});
+			};
 		}]);
 
-}).call(this, this.angular, this.H.ScoreKeeper);
+}).call(this, this.angular, this.H.ScoreKeeper, this._);
