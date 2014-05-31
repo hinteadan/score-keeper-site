6db956fbf0bae63ad099aadb284185635835e1b9
Hintea Dan Alexandru <hinteadan@gmail.com>
2014-05-31 20:29:49 +0300

    Scaffold table tennis module

---
+++ app/scripts/tableTennis/definePartiesCtrl.js
@@ -0,0 +1,15 @@
+﻿(function (angular, k) {
+	'use strict';
+
+	angular.module('ScoreKeeper.TableTennis')
+		.controller('defineParties', ['$scope', function ($scope) {
+			var parties = [
+				new k.Party(),
+				new k.Party()
+			];
+
+			$scope.parties = parties;
+
+		}]);
+
+}).call(this, this.angular, this.H.ScoreKeeper);
--- app/scripts/tableTennis/module.js
+++ app/scripts/tableTennis/module.js
@@ -1,7 +1,12 @@
 (function(angular){
 	'use strict';
 
-	angular.module('ScoreKeeper.TableTennis', []);
+	angular.module('ScoreKeeper.TableTennis', ['ngRoute'])
+	.config(['$routeProvider', function ($routeProvider) {
+		$routeProvider
+            .when('/parties', { templateUrl: 'scripts/tableTennis/defineParties.tmpl.html', controller: 'defineParties' })
+            .otherwise({ redirectTo: '/parties' });
+	}]);
 
 
 }).call(this, this.angular);
