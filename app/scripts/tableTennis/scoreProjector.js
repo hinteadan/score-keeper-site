b7704dfb2e6eabf428e04da7c9fd86855d02db62
Hintea Dan Alexandru <hinteadan@gmail.com>
2014-06-01 11:35:24 +0300

    OD: Implementing table tennis score projector

--- app/scripts/tableTennis/scoreProjector.js
+++ app/scripts/tableTennis/scoreProjector.js
@@ -10,7 +10,20 @@
 	function ScoreProjector(clash) {
 		/// <param name="clash" type="H.ScoreKeeper.Clash" />
 		
+		function projectCurrentClashState() {
+			var projection = new Projection();
 
+			angular.forEach(clash.parties, function (party) {
+				/// <param name="party" type="H.ScoreKeeper.Party" />
+				projection.scorePerPartyName[party.name] = 0;
+			});
+			projection.serving = clash.details.firstToServe;
+			projection.receiving = clash.details.firstToReceive;
+
+			return projection;
+		}
+
+		this.now = projectCurrentClashState;
 
 	}
 	ScoreProjector.Projection = Projection;
