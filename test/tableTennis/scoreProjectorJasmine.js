5fd9b8aa30f214720cb240e83d9e66fb25dca2c7
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-06-02 14:04:29 +0300

    OD: Port tests to jasmine

--- test/tableTennis/scoreProjectorJasmine.js
+++ test/tableTennis/scoreProjectorJasmine.js
@@ -54,6 +54,61 @@
                 projectionOk(0, 0, parties[0].individuals[0], parties[1].individuals[0]);
             });
 
+            it('projects correctly when 1 point is scored', function () {
+                score().for(clash.parties[0]);
+                projectionOk(1, 0, parties[0].individuals[0], parties[1].individuals[0]);
+            });
+
+            it('projects correctly when 4 points are scored', function () {
+                score(3).for(clash.parties[0]);
+                score(1).for(clash.parties[1]);
+
+                projectionOk(3, 1, parties[0].individuals[0], parties[1].individuals[0]);
+            });
+
+            it('projects correctly when 5 points are scored', function () {
+                score(3).for(clash.parties[0]);
+                score(2).for(clash.parties[1]);
+
+                projectionOk(3, 2, parties[1].individuals[0], parties[0].individuals[1]);
+            });
+
+            it('projects correctly when 10 points are scored', function () {
+                score(6).for(clash.parties[0]);
+                score(4).for(clash.parties[1]);
+
+                projectionOk(6, 4, parties[0].individuals[1], parties[1].individuals[1]);
+            });
+
+            it('projects correctly when playing on tie-break', function () {
+                score(10).for(clash.parties[0]);
+                score(10).for(clash.parties[1]);
+                projectionOk(10, 10, parties[0].individuals[0], parties[1].individuals[0]);
+                score().for(clash.parties[0]);
+                projectionOk(11, 10, parties[1].individuals[0], parties[0].individuals[1]);
+                score().for(clash.parties[1]);
+                projectionOk(11, 11, parties[0].individuals[1], parties[1].individuals[1]);
+                score().for(clash.parties[1]);
+                projectionOk(11, 12, parties[1].individuals[1], parties[0].individuals[0]);
+                score().for(clash.parties[1]);
+                projectionOk(11, 13, parties[0].individuals[0], parties[1].individuals[0]);
+            });
+
+            it('projects correctly on straight win', function () {
+                score(11).for(clash.parties[0]);
+                projectionWonBy(clash.parties[0]);
+            });
+
+            it('projects correctly when winning on tie-break', function () {
+                score(10).for(clash.parties[0]);
+                score(11).for(clash.parties[1]);
+                expect(projector.now().isWon).toBe(false);
+                score().for(clash.parties[0]);
+                expect(projector.now().isWon).toBe(false);
+                score(2).for(clash.parties[1]);
+                projectionWonBy(clash.parties[1]);
+            });
+
         });
 
     });
