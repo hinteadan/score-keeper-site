3493550ca569c233ef4edef03159014bfd2defe2
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-06-02 14:09:18 +0300

    OD: Port tests to jasmine

--- test/tableTennis/scoreProjectorJasmine.js
+++ test/tableTennis/scoreProjectorJasmine.js
@@ -34,12 +34,13 @@
     }
 
     describe('Table Tennis scoring', function () {
+
         it('uses inject-ables', function () {
             expect(ScoreProjector).toBeDefined();
             expect(clashService).toBeDefined();
         });
 
-        describe('For doubles', function () {
+        describe('For Doubles', function () {
 
             beforeEach(function () {
                 parties = [
@@ -107,6 +108,63 @@
                 expect(projector.now().isWon).toBe(false);
                 score(2).for(clash.parties[1]);
                 projectionWonBy(clash.parties[1]);
+            });
+
+        });
+
+        describe('For Singles', function () {
+
+            beforeEach(function () {
+                parties = [
+				new k.Party('Team Hintea').addMembers([new k.Individual('Hintea', 'Dan')]),
+				new k.Party('Team Gicu').addMembers([new k.Individual('Orian', 'Georgian')])
+                ];
+                clash = new k.Clash(parties, new clashService.ClashDetails(11, parties[0].individuals[0], parties[1].individuals[0]));
+                projector = new ScoreProjector(clash);
+            });
+
+            it('initially projects according to clash definition', function () {
+                projectionOk(0, 0, parties[0].individuals[0], parties[1].individuals[0]);
+            });
+
+            it('projects correctly when 1 point is scored', function () {
+                score().for(clash.parties[0]);
+                projectionOk(1, 0, parties[0].individuals[0], parties[1].individuals[0]);
+            });
+
+            it('projects correctly when 4 points are scored', function () {
+                score(3).for(clash.parties[0]);
+                score(1).for(clash.parties[1]);
+
+                projectionOk(3, 1, parties[0].individuals[0], parties[1].individuals[0]);
+            });
+
+            it('projects correctly when 5 points are scored', function () {
+                score(3).for(clash.parties[0]);
+                score(2).for(clash.parties[1]);
+
+                projectionOk(3, 2, parties[1].individuals[0], parties[0].individuals[0]);
+            });
+
+            it('projects correctly when 10 points are scored', function () {
+                score(6).for(clash.parties[0]);
+                score(4).for(clash.parties[1]);
+
+                projectionOk(6, 4, parties[0].individuals[0], parties[1].individuals[0]);
+            });
+
+            it('projects correctly when playing on tie-break', function () {
+                score(10).for(clash.parties[0]);
+                score(10).for(clash.parties[1]);
+                projectionOk(10, 10, parties[0].individuals[0], parties[1].individuals[0]);
+                score().for(clash.parties[0]);
+                projectionOk(11, 10, parties[1].individuals[0], parties[0].individuals[0]);
+                score().for(clash.parties[1]);
+                projectionOk(11, 11, parties[0].individuals[0], parties[1].individuals[0]);
+                score().for(clash.parties[1]);
+                projectionOk(11, 12, parties[1].individuals[0], parties[0].individuals[0]);
+                score().for(clash.parties[1]);
+                projectionOk(11, 13, parties[0].individuals[0], parties[1].individuals[0]);
             });
 
         });
