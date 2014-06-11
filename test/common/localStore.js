2f0d9d8c862604da19ee7a5feb37b479c5cdfbbb
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-06-11 18:09:28 +0300

    Test for storing complex types

--- test/common/localStore.js
+++ test/common/localStore.js
@@ -3,13 +3,22 @@
 
     var store = new LocalStore('UnitTests-6821EE21-0693-464B-9E75-204A7AD5CB7F');
 
+    function fullyRefreshLocalStore() {
+        store = new LocalStore('UnitTests-6821EE21-0693-464B-9E75-204A7AD5CB7F');
+    }
+
     describe('Local Store', function () {
+
+        beforeEach(function () {
+            store = new LocalStore('UnitTests-6821EE21-0693-464B-9E75-204A7AD5CB7F');
+        });
 
         afterEach(function () {
             store.purge();
         });
 
         it('starts off empty', function () {
+            fullyRefreshLocalStore();
             expect(store.query()).toBeDefined();
             expect(store.query().length).toBe(0);
         });
@@ -20,11 +29,37 @@
             store.add(true);
             store.add(false);
             store.add('some string');
+            fullyRefreshLocalStore();
             expect(store.query()[0]).toBe(null);
             expect(store.query()[1]).toBe(42);
             expect(store.query()[2]).toBe(true);
             expect(store.query()[3]).toBe(false);
             expect(store.query()[4]).toBe('some string');
+        });
+
+        it('stores complex entities', function () {
+            var entity = {
+                id: 'entity-1',
+                name: 'Some awesome entity',
+                metadata: {
+                    param1: 'awesome',
+                    param2: 0,
+                    param3: false,
+                    param4: true,
+                    param5: new Date()
+                },
+                details: {
+                    person: {
+                        firstName: 'Dan',
+                        lastName: 'Hintea'
+                    },
+                    timestamp: new Date(),
+                    roles: ['role1', 'role2', 'role3']
+                }
+            };
+            store.add(entity);
+            fullyRefreshLocalStore();
+            expect(store.query()[0]).toEqual(entity);
         });
 
     });
