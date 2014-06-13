4594152d8b991ac8c586b6d4911eb699066df6c6
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-06-13 14:10:09 +0300

    Redirect to proper location after session restore

--- app/scripts/tableTennis/restoreCtrl.js
+++ app/scripts/tableTennis/restoreCtrl.js
@@ -1,9 +1,36 @@
-﻿(function (angular, confirm) {
+﻿(function (angular, confirm, _) {
 	'use strict';
 
 	angular.module('ScoreKeeper.TableTennis')
-	.controller('restore', ['$scope', 'Clash', 'ClashLocalStore', function ($scope, clash, clashStore) {
+	.controller('restore', ['$scope', '$location', 'Clash', 'ClashLocalStore', function ($scope, $location, clash, clashStore) {
 		/// <param name='clashStore' type='storage.LocalStore' />
+
+	    function isPartyNameless(p) {
+	        return p.name.replace(/\s/gi, '') === '' ? true : false;
+	    }
+
+	    function isPartyEmpty(p) {
+	        return p.individuals.length === 0;
+	    }
+
+	    function isPartyNamelessOrEmpty(p) {
+	        return isPartyNameless(p) || isPartyEmpty(p);
+	    }
+
+	    function goToCurrentClashState() {
+	        if (_.any(clash.parties, isPartyNamelessOrEmpty)) {
+	            $location.path('/parties');
+	            return;
+	        }
+	        if (!clash.details.isValid() || !clash.details.hasBegun()) {
+	            $location.path('/clash');
+	            return;
+	        }
+	        if (!clash.details.hasEnded()) {
+	            $location.path('/play');
+	            return;
+	        }
+	    }
 
 		$scope.any = clashStore.any;
 		$scope.clashes = clashStore.query();
@@ -13,6 +40,7 @@
 			}
 			clash.revive(dto);
 			clashStore.replace(dto).with(clash);
+			goToCurrentClashState();
 		};
 		$scope.purgeAll = function () {
 		    if (!confirm('Are you sure you want to permanently delete all the saved sessions?')) {
@@ -23,4 +51,4 @@
 
 	}]);
 
-}).call(this, this.angular, this.confirm);
+}).call(this, this.angular, this.confirm, this._);
