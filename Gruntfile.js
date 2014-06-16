c8da4d51350f6914f0e4cfc92ec0e30a4fb178f5
Hintea Dan Alexandru <hinteadan@gmail.com>
2014-06-16 22:09:20 +0300

    Remove ngmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -244,20 +244,6 @@ module.exports = function (grunt) {
 			}
 		},
 
-		// ngmin tries to make the code safe for minification automatically by
-		// using the Angular long form for dependency injection. It doesn't work on
-		// things like resolve or inject so those have to be done manually.
-		ngmin: {
-			dist: {
-				files: [{
-					expand: true,
-					cwd: '.tmp/concat/scripts',
-					src: '*.js',
-					dest: '.tmp/concat/scripts'
-				}]
-			}
-		},
-
 		// Replace Google CDN references
 		cdnify: {
 			dist: {
@@ -421,7 +407,6 @@ module.exports = function (grunt) {
       'concurrent:dist',
       'autoprefixer',
       'concat',
-      //'ngmin',
       'copy:dist',
       //'cdnify',
       'cssmin',
