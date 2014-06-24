acafc0e870528955218e56c5102361ce1920cb20
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-06-24 09:53:35 +0300

    Navigation

---
+++ app/scripts/tableTennis/navigationCtrl.js
@@ -0,0 +1,23 @@
+﻿(function (angular) {
+    'use strict';
+
+    angular.module('ScoreKeeper.TableTennis')
+    .controller('navigation', ['$scope', '$window', '$timeout', function ($s, $w, $t) {
+
+        $s.goHome = function () {
+            if (!$s.goHome.confirm) {
+                $s.goHome.confirm = true;
+                $t(function () {
+                    delete $s.goHome.confirm;
+                }, 5000);
+                return;
+            }
+
+            delete $s.goHome.confirm;
+
+            $w.location.href = 'index.html';
+        };
+
+    }]);
+
+}).call(this, this.angular);
