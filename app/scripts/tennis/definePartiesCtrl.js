0139ecec4d198fbb95648213ab0f64db5024bdb6
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-06-25 10:48:25 +0300

    Scaffold tennis module

---
+++ app/scripts/tennis/definePartiesCtrl.js
@@ -0,0 +1,9 @@
+﻿(function (angular) {
+    'use strict';
+
+    angular.module('ScoreKeeper.Tennis')
+		.controller('defineParties', [function () {
+		    
+		}]);
+
+}).call(this, this.angular);
---
+++ app/scripts/tennis/module.js
@@ -0,0 +1,11 @@
+﻿(function (angular) {
+    'use strict';
+
+    angular.module('ScoreKeeper.Tennis', ['ngRoute'])
+	.config(['$routeProvider', function ($routeProvider) {
+	    $routeProvider
+			.when('/parties', { templateUrl: 'scripts/tennis/defineParties.tmpl.html', controller: 'defineParties' })
+			.otherwise({ redirectTo: '/parties' });
+	}]);
+
+}).call(this, this.angular);
