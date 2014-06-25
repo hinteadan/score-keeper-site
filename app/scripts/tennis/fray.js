ee0b484fd2f332d49bb338c5e66b4eb395a9b031
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-06-25 11:43:11 +0300

    Define Tennis fray details

---
+++ app/scripts/tennis/fray.js
@@ -0,0 +1,52 @@
+﻿(function (angular) {
+    'use strict';
+
+    var gameTieMode = {
+            advantageWin: 'AdvantageWin',
+            singlePointWin: 'SinglePointWin'
+        },
+        setTieMode = {
+            tieBreak: 'TieBreak',
+            superTieBreak: 'SuperTieBreak',
+            gameDifference: 'GameDifference'
+        };
+
+    function SetDetails() {
+        this.firstToServe = null;
+        this.firstToReceive = null;
+        this.tieMode = setTieMode.tieBreak;
+    }
+
+    function FrayDetails() {
+        this.firstToServe = null;
+        this.firstToReceive = null;
+
+        this.gamesPerSet = 6;
+        this.setsToWin = 2;
+
+        this.gameTieMode = gameTieMode.advantageWin;
+        this.lastSetTieMode = setTieMode.tieBreak;
+
+        this.createdOn = new Date();
+        this.startedOn = null;
+        this.endedOn = null;
+
+        this.isValid = function () {
+            return this.firstToServe && this.firstToReceive;
+        };
+        this.hasBegun = function () {
+            return this.startedOn !== null;
+        };
+        this.hasEnded = function () {
+            return this.endedOn !== null;
+        };
+    }
+
+    angular.module('ScoreKeeper.Tennis')
+	.service('Fray', [function () {
+
+
+
+	}]);
+
+}).call(this, this.angular);
