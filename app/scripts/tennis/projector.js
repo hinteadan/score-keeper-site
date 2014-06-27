c8e9c467275b13d92b61147e98cb3a2c8f4ddc65
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-06-27 12:57:05 +0300

    OD: Tennis Game Projection tests

--- app/scripts/tennis/projector.js
+++ app/scripts/tennis/projector.js
@@ -1,4 +1,4 @@
-﻿(function (angular) {
+﻿(function (angular, _) {
     'use strict';
 
     
@@ -7,17 +7,30 @@
         /// <param name='clash' type='H.ScoreKeeper.Clash' />
 
         function projectCurrentState() {
-
+            var projection = new GameProjector.Projection();
+            _.each(clash.parties, function (p) {
+                projection.scorePerPartyName[p.name] = '0';
+            });
+            return projection;
         }
 
         this.now = projectCurrentState;
     }
+    GameProjector.Projection = function () {
+        this.scorePerPartyName = {};
+        this.serving = null;
+        this.receiving = null;
+        this.isWon = false;
+        this.winner = null;
+    };
+
 
     function Projector() {
         
     }
+    Projector.Game = GameProjector;
 
     angular.module('ScoreKeeper.Tennis')
     .value('Projector', Projector);
 
-}).call(this, this.angular);
+}).call(this, this.angular, this._);
--- test/tennis/projector.js
+++ test/tennis/projector.js
@@ -1,15 +1,37 @@
-﻿(function (angular) {
+﻿(function (angular, k) {
     'use strict';
 
     var inject = angular.injector(['ScoreKeeper.Tennis']),
-		fray = inject.get('Fray'),
-        projector = inject.get('Projector');
+		MatchDetails = inject.get('FrayDetails'),
+        Projector = inject.get('Projector'),
+        parties = null,
+        clash = null,
+		p = null;
 
     describe('Tennis scoring', function () {
         it('uses inject-ables', function () {
-            expect(fray).toBeDefined();
-            expect(projector).toBeDefined();
+            expect(MatchDetails).toBeDefined();
+            expect(Projector).toBeDefined();
         });
+
+        describe('Game scoring', function () {
+            beforeEach(function () {
+                parties = [
+				    new k.Party('Fed').addMembers([new k.Individual('Roger', 'Federer')]),
+				    new k.Party('Rafa').addMembers([new k.Individual('Rafael', 'Nadal')])
+                ];
+                clash = new k.Clash(parties);
+                p = new Projector.Game(clash);
+            });
+
+            it('begins at love all', function () {
+                var proj = p.now();
+                expect(proj.scorePerPartyName['Fed']).toEqual('0');
+                expect(proj.scorePerPartyName['Rafa']).toEqual('0');
+            });
+
+        });
+
     });
 
-}).call(this, this.angular);
+}).call(this, this.angular, this.H.ScoreKeeper);
