b0dca06ff660965e285942ed6930537f55501b14
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-06-27 14:18:47 +0300

    OD: Tennis game scoring projection

--- app/scripts/tennis/projector.js
+++ app/scripts/tennis/projector.js
@@ -1,16 +1,30 @@
 ﻿(function (angular, _) {
     'use strict';
 
-    
+    var gameScore = ['0', '15', '30', '40'],
+        gameScoreOnDifference = ['40', 'Ad', 'Wn'];
 
     function GameProjector(clash) {
         /// <param name='clash' type='H.ScoreKeeper.Clash' />
 
+        function gameScoreFor(pointScore, otherPointScore) {
+            var diff = pointScore - otherPointScore;
+            if (pointScore < 3) {
+                return gameScore[pointScore];
+            }
+            else if (pointScore === 3) {
+                return diff < 0 ? '-' : gameScore[pointScore];
+            }
+            return diff < 0 ? '-' : gameScoreOnDifference[diff];
+        }
+
         function projectCurrentState() {
-            var projection = new GameProjector.Projection();
-            _.each(clash.parties, function (p) {
-                projection.scorePerPartyName[p.name] = '0';
-            });
+            var projection = new GameProjector.Projection(),
+                fed = clash.parties[0],
+                rafa = clash.parties[1];
+            projection.scorePerPartyName[fed.name] = gameScoreFor(clash.scoreFor(fed), clash.scoreFor(rafa));
+            projection.scorePerPartyName[rafa.name] = gameScoreFor(clash.scoreFor(rafa), clash.scoreFor(fed));
+            
             return projection;
         }
 
