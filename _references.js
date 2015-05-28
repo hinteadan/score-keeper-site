5b94caa8d20a152596316b4480bd4ae836e74769
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2015-05-28 11:17:26 +0300

    OD: A little cleanup and refactoring

--- _references.js
+++ _references.js
@@ -1,10 +1,33 @@
-﻿/// <reference path="app/bower_components/h.scoreKeeper/h.scoreKeeper.js" />
-/// <reference path="app/bower_components/lodash/dist/lodash.js" />
-/// <reference path="app/bower_components/jquery.gracket/jquery.gracket.js" />
-/// <reference path="app/scripts/tournamentWizard.js" />
-/// <reference path="app/bower_components/angular/angular.js" />
-/// <reference path="app/bower_components/h.dataStore/h.dataStore.js" />
+﻿/// <reference path="~/app/bower_components/jquery/dist/jquery.js" />
+/// <reference path="~/app/bower_components/lodash/dist/lodash.js" />
+/// <reference path="~/app/bower_components/jquery.gracket/jquery.gracket.js" />
+/// <reference path="~/app/scripts/tournamentWizard.js" />
+/// <reference path="~/app/bower_components/angular/angular.js" />
+/// <reference path="~/app/bower_components/jasmine/lib/jasmine-core/jasmine.js" />
+/// <reference path="~/app/bower_components/jasmine/lib/jasmine-core/boot/boot.js" />
+/// <reference path="~/app/bower_components/h.scoreKeeper/h.scoreKeeper.js" />
+/// <reference path="~/app/bower_components/h.dataStore/h.dataStore.js" />
 
-/// <reference path="app/bower_components/jasmine/lib/jasmine-core/jasmine.js" />
-/// <reference path="app/bower_components/jasmine/lib/jasmine-core/boot/boot.js" />
-/// <reference path="app/scripts/common/localStore.js" />
+/// <reference path="~/app/scripts/common/localStore.js" />
+/// <reference path="~/app/scripts/common/module.js" />
+
+
+/// <reference path="~/app/scripts/tableTennis/clash.js" />
+/// <reference path="~/app/scripts/tableTennis/clashLocalStore.js" />
+/// <reference path="~/app/scripts/tableTennis/clashStateRouter.js" />
+/// <reference path="~/app/scripts/tableTennis/dataStore.js" />
+/// <reference path="~/app/scripts/tableTennis/pointDetails.js" />
+/// <reference path="~/app/scripts/tableTennis/scoreProjector.js" />
+
+/// <reference path="~/app/scripts/tableTennis/module.js" />
+
+
+/// <reference path="~/app/scripts/tennis/frayDetails.js" />
+/// <reference path="~/app/scripts/tennis/fray.js" />
+/// <reference path="~/app/scripts/tennis/projector.js" />
+/// <reference path="~/app/scripts/tennis/module.js" />
+
+
+
+/// <reference path="~/app/scripts/tournamentWizard.js" />
+/// <reference path="~/app/scripts/app.js" />
--- app/scripts/tableTennis/clash.js
+++ app/scripts/tableTennis/clash.js
@@ -48,110 +48,108 @@
         };
     }
 
-    function ClashService(ScoreProjector) {
-
-        var parties = [
-				new k.Party(),
-				new k.Party()
-            ],
-			clashDetails = new ClashSetDetails(),
-			clashSet = null,
-			projector = null;
-
-        function detailsForClashIndex(index) {
-
-            function other(party, member){
-                if(!member){
-                    return parties[0] === party ? parties[1] : parties[0];
-                }
-                return party.individuals[0] === member ? party.individuals[1] : party.individuals[0];
-            }
-
-            var firstServeParty = _.find(parties, function (p) { return _.contains(p.individuals, clashDetails.firstToServe); }),
-                firstReceivingParty = other(firstServeParty),
-                singles = [
-                    new ClashDetails(clashDetails.firstToServe, clashDetails.firstToReceive),
-                    new ClashDetails(clashDetails.firstToReceive, clashDetails.firstToServe)
-                ],
-                doubles = [
-                    new ClashDetails(clashDetails.firstToServe, clashDetails.firstToReceive),
-                    new ClashDetails(clashDetails.firstToReceive, other(firstServeParty, clashDetails.firstToServe)),
-                    new ClashDetails(other(firstServeParty, clashDetails.firstToServe), other(firstReceivingParty, clashDetails.firstToReceive)),
-                    new ClashDetails(other(firstReceivingParty, clashDetails.firstToReceive), clashDetails.firstToServe),
-                ],
-                isSinglesClash = firstServeParty.individuals.length === 1;
-
-            return isSinglesClash ? singles[index % 2] : doubles[index % 4];
-        }
-
-        function ensureClashSet() {
-            if (clashSet) {
-                return;
-            }
-            clashSet = new k.ClashSet(
-					_.map(_.range(0, clashDetails.setsToWin * 2 - 1, 1), function (i) { return new k.Clash(parties, detailsForClashIndex(i)); }),
-					parties,
-					clashDetails
-				);
-        }
-
-        function ensureScoreProjector() {
-            ensureClashSet();
-            if (projector) {
-                return;
-            }
-            projector = new ScoreProjector(clashSet);
-        }
-
-        this.parties = parties;
-        this.theOtherParty = function (currentParty) {
-            return currentParty === parties[0] ? parties[1] : parties[0];
-        };
-        this.details = clashDetails;
-        this.ClashDetails = ClashSetDetails;
-        this.clashSet = function () {
-            ensureClashSet();
-            this.skClashSet = clashSet;
-            return clashSet;
-        };
-
-        this.projectScore = function () {
-            ensureScoreProjector();
-            return projector;
-        };
-        this.play = function () {
-            clashDetails.startedOn = new Date();
-        };
-        this.stop = function () {
-            clashDetails.endedOn = new Date();
-        };
-
-        this.revive = function (dto) {
-            var memberPool = [];
-            if (this === dto) {
-                return;
-            }
-            clearArray(parties);
-            _.each(dto.parties, function (p) { parties.push(k.Party.revive(p)); });
-            memberPool = _.flatten(_.pluck(parties, 'individuals'));
-            clashDetails.revive(dto.details, memberPool);
-
-            if (dto.skClashSet) {
-                clashSet = k.ClashSet.revive(dto.skClashSet, parties,
-                    function () { return clashDetails; },
-                    function (dto) {
-                        return new ClashDetails(
-                            _.find(memberPool, function (m) { return m.isMatching(dto.firstToServe); }),
-                            _.find(memberPool, function (m) { return m.isMatching(dto.firstToReceive); })
-                            );
-                    });
-                this.skClashSet = clashSet;
-                projector = new ScoreProjector(clashSet);
-            }
-        };
-    }
-
     angular.module('ScoreKeeper.TableTennis')
-		.service('Clash', ['ScoreProjector', ClashService]);
+		.service('Clash', ['ScoreProjector', function ClashService(ScoreProjector) {
+
+		    var parties = [
+                    new k.Party(),
+                    new k.Party()
+		    ],
+                clashDetails = new ClashSetDetails(),
+                clashSet = null,
+                projector = null;
+
+		    function detailsForClashIndex(index) {
+
+		        function other(party, member) {
+		            if (!member) {
+		                return parties[0] === party ? parties[1] : parties[0];
+		            }
+		            return party.individuals[0] === member ? party.individuals[1] : party.individuals[0];
+		        }
+
+		        var firstServeParty = _.find(parties, function (p) { return _.contains(p.individuals, clashDetails.firstToServe); }),
+                    firstReceivingParty = other(firstServeParty),
+                    singles = [
+                        new ClashDetails(clashDetails.firstToServe, clashDetails.firstToReceive),
+                        new ClashDetails(clashDetails.firstToReceive, clashDetails.firstToServe)
+                    ],
+                    doubles = [
+                        new ClashDetails(clashDetails.firstToServe, clashDetails.firstToReceive),
+                        new ClashDetails(clashDetails.firstToReceive, other(firstServeParty, clashDetails.firstToServe)),
+                        new ClashDetails(other(firstServeParty, clashDetails.firstToServe), other(firstReceivingParty, clashDetails.firstToReceive)),
+                        new ClashDetails(other(firstReceivingParty, clashDetails.firstToReceive), clashDetails.firstToServe),
+                    ],
+                    isSinglesClash = firstServeParty.individuals.length === 1;
+
+		        return isSinglesClash ? singles[index % 2] : doubles[index % 4];
+		    }
+
+		    function ensureClashSet() {
+		        if (clashSet) {
+		            return;
+		        }
+		        clashSet = new k.ClashSet(
+                        _.map(_.range(0, clashDetails.setsToWin * 2 - 1, 1), function (i) { return new k.Clash(parties, detailsForClashIndex(i)); }),
+                        parties,
+                        clashDetails
+                    );
+		    }
+
+		    function ensureScoreProjector() {
+		        ensureClashSet();
+		        if (projector) {
+		            return;
+		        }
+		        projector = new ScoreProjector(clashSet);
+		    }
+
+		    this.parties = parties;
+		    this.theOtherParty = function (currentParty) {
+		        return currentParty === parties[0] ? parties[1] : parties[0];
+		    };
+		    this.details = clashDetails;
+		    this.ClashDetails = ClashSetDetails;
+		    this.clashSet = function () {
+		        ensureClashSet();
+		        this.skClashSet = clashSet;
+		        return clashSet;
+		    };
+
+		    this.projectScore = function () {
+		        ensureScoreProjector();
+		        return projector;
+		    };
+		    this.play = function () {
+		        clashDetails.startedOn = new Date();
+		    };
+		    this.stop = function () {
+		        clashDetails.endedOn = new Date();
+		    };
+
+		    this.revive = function (dto) {
+		        var memberPool = [];
+		        if (this === dto) {
+		            return;
+		        }
+		        clearArray(parties);
+		        _.each(dto.parties, function (p) { parties.push(k.Party.revive(p)); });
+		        memberPool = _.flatten(_.pluck(parties, 'individuals'));
+		        clashDetails.revive(dto.details, memberPool);
+
+		        if (dto.skClashSet) {
+		            clashSet = k.ClashSet.revive(dto.skClashSet, parties,
+                        function () { return clashDetails; },
+                        function (dto) {
+                            return new ClashDetails(
+                                _.find(memberPool, function (m) { return m.isMatching(dto.firstToServe); }),
+                                _.find(memberPool, function (m) { return m.isMatching(dto.firstToReceive); })
+                                );
+                        });
+		            this.skClashSet = clashSet;
+		            projector = new ScoreProjector(clashSet);
+		        }
+		    };
+		}]);
 
 }).call(this, this.angular, this.H.ScoreKeeper, this._);
